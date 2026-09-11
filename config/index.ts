11b7e11f835d3097ef8646429cd5c41c4d6e7023
Yalin814 <13203701351@163.com>
2026-09-11 14:01:56 +0800

    fix: change public path to /kado/

--- config/index.ts
+++ config/index.ts
@@ -53,7 +53,7 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
     },
     h5: {
       esnextModules: [/@antmjs[\/]vantui/],
-      publicPath: '/',
+      publicPath: '/kado/',
       staticDirectory: 'static',
       output: {
         filename: 'js/[name].[hash:8].js',
